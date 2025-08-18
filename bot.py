e449e4b55f7a108e4adfdf9cd7a21c92a68cded5
ftmbotzx <kumarasta91@gmail.com>
2025-08-18 16:50:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
@@ -13,7 +10,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="TechVJ"),
+            plugins=dict(root="FTM DEV"),
             workers=50,
             sleep_threshold=10
         )
@@ -22,15 +19,11 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('🦉 Bot Started Powered By @ftmbotzx')
 
     async def stop(self, *args):
 
         await super().stop()
-        print('Bot Stopped Bye')
+        print('🥱😴 Bot Stopped Bye')
 
 Bot().run()
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
