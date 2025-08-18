f6ca422cf58ec288e474d0aa9a877bd6b535e142
ftmbotzx <kumarasta91@gmail.com>
2025-08-18 19:28:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="FTM DEV"),
+            plugins=dict(root="FTMDev"),
             workers=50,
             sleep_threshold=10
         )
